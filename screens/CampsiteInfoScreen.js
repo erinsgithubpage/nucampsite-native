9f7853b9b12475544d611f1277a7622cd15bdcd6
erinh <erin.hazelwood@outlook.com>
2023-03-26 06:07:59 -0400

    Icons and More, video one completed

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -7,6 +7,7 @@ const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
   
   const [comments, setComments] = useState(COMMENTS);
+  const [favorite, setFavorite] = useState(false);
 
   const renderCommentItem = ({ item }) => {
     return (
@@ -14,7 +15,7 @@ const CampsiteInfoScreen = ({ route }) => {
         <Text style={{ fontSize: 14 }}>{item.text}</Text>
         <Text style={{ fontSize: 12 }}>Stars</Text>
         <Text style={{ fontSize: 12 }}>
-          {`  ${item.author}, ${item.date}`}
+          {`--${item.author}, ${item.date}`}
         </Text>
       </View>
     )
@@ -23,23 +24,43 @@ const CampsiteInfoScreen = ({ route }) => {
   return (
     <FlatList
       data={comments.filter(
-        (comment) => commment.campsiteId === campsite.id
+        (comment) => comment.campsiteId === campsite.id
       )}
       renderItem={renderCommentItem}
       keyExtractor={(item) => item.id.toString()}
       contentContainerStyle={{
         marginHorizontal: 20,
         paddingVertical: 20
-
       }}
       ListHeaderComponent={
         <>
-          <RenderCampsite campsite={campsite} />
+          <RenderCampsite 
+            campsite={campsite} 
+            isFavorite={favorite}
+            markFavorite={() => setFavorite(true) }
+          />
           <Text style={StyleSheet.commentsTitle}>Comments</Text>
         </>
       }
       />
   )
 };
+
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: 'center',
+    backgroundColor: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#43484D',
+    padding: 10,
+    paddingTop: 30
+  },
+  commentItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff'
+  }
+})
 
 export default CampsiteInfoScreen;
