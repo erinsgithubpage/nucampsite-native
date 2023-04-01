0860b942e01c4f1dd77e9d16016b806c1d19776c
erinh <erin.hazelwood@outlook.com>
2023-04-01 06:40:37 -0400

    task one, step four

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -53,7 +53,24 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignitems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0,0,0,1)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
     }
 });
+
+
 
 export default RenderCampsite;
